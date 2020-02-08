76a74e2dbf9f5b0f58f5a1969b0d4ea5cd227e78
badra <abnreddy.a@gmail.com>
2020-02-08 22:43:38 +0530

    initial commit

---
+++ Sorting Algorithms/merge_sort.py
@@ -0,0 +1,31 @@
+
+"""
+What is merge sort?
+Mergesort is an efficient, general purpose and comparision based algorithm.
+It is a devide and conquer algorithm.
+
+
+Data structure          : Array/List
+Worst-case performance  : O(n log n)
+Best-case performance   : O(n log n) typical, O(n) natural variant
+Average performance     : O(n log n)
+Worst-case space complexity : О(n) total with O(n) auxiliary, O(1) auxiliary with linked lists
+
+Example:
+    data = [20,4,5,3,25,6,667,443,888]
+    sorted_data = [3, 4, 5, 6, 20, 25, 443, 443, 888]
+
+"""
+
+
+def merge_sort(data):
+    pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    data = [20, 4, 5, 3, 25, 6, 667, 443, 888]
+    merge_sort(data)
